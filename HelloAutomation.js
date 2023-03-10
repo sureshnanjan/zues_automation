43e1db65c5185cf81a6e01e64235ef42d036c8ac
sureshnanjan <sureshnanjan@outlook.com>
2023-03-10 09:44:02 +0530

    Doing the Changes to project

--- HelloAutomation.js
+++ HelloAutomation.js
@@ -1,1 +1,22 @@
+/**
+ * This is Doc Examples
+ */
 console.log("Automating Heroku App")
+
+/**
+ * 
+ * @param {*} url 
+ */
+function openHomePage(url){
+    // Perform some operations
+}
+
+/**
+ * This is the type that holding the details of the Heroku App Home Page
+ */
+class HomePage{
+    constructor(){
+        this.Heading = "Identifier for the Heading"
+        this.SubHeading = "Identifier for the Sub Heading on the home page"
+    }
+}
