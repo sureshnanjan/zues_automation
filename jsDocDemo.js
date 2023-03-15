290fdca9298d38ae724ee9763502a8ce4a8bea02
Rahul Gupta <rahul.gupta@zeuslearning.mumbai>
2023-03-15 10:17:56 +0530

    js doc commit

---
+++ jsDocDemo.js
@@ -0,0 +1,7 @@
+/** This is a description of the consoleMessage function. 
+ * @param {string} message The message you want to print to the console
+*/
+
+function consoleMessage(message) {
+    console.log(message)
+}
