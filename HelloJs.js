77216b639893d44165c3498d4b2c47fd15ebfbf1
neha.qazi <neha.qazi@zeuslearning.com>
2023-03-15 10:48:37 +0530

    jsdoc activity commit

---
+++ HelloJs.js
@@ -0,0 +1,14 @@
+console.log("Learning automation using Playwright")
+
+
+
+/**
+ * This is my first function in an automation script
+ * @param {string} para1 this the first parameter
+ * @param {number} para2 this the second parameter
+ */
+function myFirstFunction(para1, para2){
+    return 0;
+}
+
+myFirstFunction.para1
