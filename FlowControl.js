6ff78a2b92556c03e0d6153408743d3b7ffd9182
sureshnanjan <sureshnanjan@outlook.com>
2023-03-15 10:08:34 +0530

    Changes in class demo

---
+++ FlowControl.js
@@ -0,0 +1,20 @@
+if (condition) {
+    // Statements for when condition is true
+    // …
+  } else {
+    // Statements for when condition is false
+    // …
+  }
+  
+switch (expression) {
+    case label1:
+      statements1;
+      break;
+    case label2:
+      statements2;
+      break;
+    // …
+    default:
+      statementsDefault;
+  }
+  
