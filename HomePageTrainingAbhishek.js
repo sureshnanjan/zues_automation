9bdeed859ce8276d99f1a47a790f362af1ce230c
Abhishek Singh <abhishek.singh@zeuslearning.mumbai>
2023-03-15 10:26:58 +0530

    JSDoc Training Example

---
+++ HomePageTrainingAbhishek.js
@@ -0,0 +1,14 @@
+let Heading = "This is the Heading for the HomePage";
+let SubHeading = "This is the SubHeading for the HomePage";
+
+/**
+ * This is a Demo for understanding JSDoc
+ * @param {string} param1 This is the name of the first parameter
+ * @param {number} param2 This is the number of times the value should print
+ */
+
+function abhishekDemoForJSDoc (param1, param2) {
+    return 50;
+}
+
+abhishekDemoForJSDoc("Number", 4);
