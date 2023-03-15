3c47009a77b1b4e3438385e068b804550b20abe9
Rutuja Jadhav <rutuja.jadhav@zeuslearning.mumbai>
2023-03-15 10:18:39 +0530

    Added demo method in HelloAutomation.js file for JSDoc Understanding

--- HelloAutomation.js
+++ HelloAutomation.js
@@ -20,3 +20,15 @@ class HomePage{
         this.SubHeading = "Identifier for the Sub Heading on the home page"
     }
 }
+
+/**
+ * This is a demo method for JSDoc Understanding
+ * @param {string} param1 This is name of first parameter
+ * @param {number} param2 This is numer of times the value should print
+ */
+
+function rutujaDemoforJSDoc(param1,param2){
+    return 100;
+}
+
+rutujaDemoforJSDoc("First Parameter",2);
