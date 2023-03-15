f075d7ccd093207cffb7f3fb047b4fb47bb45c48
niraj.gupta <niraj.gupta@zeuslearning.com>
2023-03-15 10:30:21 +0530

    checkboxes

---
+++ Checkboxes.js
@@ -0,0 +1,21 @@
+/**
+ * This is Doc Examples
+ */
+console.log("Automating Heroku App")
+
+/**
+ * 
+ * @param {*} url 
+ */
+function openCheckboxes(url){
+    // Perform operation
+}
+
+/**
+ * This is to click checkboxes
+ */
+class Checkboxes{
+    constructor(){
+        this.Heading = "Checkboxes";
+    }
+}
