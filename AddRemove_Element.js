74f2cf29c082a2b88e2cf56e638ff77c912101ae
Pooja Pedamkar <pooja.pedamka@zeuslearning.mumbai>
2023-03-15 09:33:14 +0530

    Added New JS File

---
+++ AddRemove_Element.js
@@ -0,0 +1,33 @@
+/**
+ * This is Doc Examples
+ */
+console.log("Automating Add/Remove Page of Heroku App")
+
+/**
+ * This method adds the element on the page
+ * @param {string} action1 
+ * @returns {number}
+ */
+
+function addElement(action1){
+   return 0;
+}
+
+/**
+ * This method deletes the element from the page
+ * @param {string} action2 
+ * @returns {number}
+ */
+function removeElement(action2){
+   return 1;
+}
+
+/**
+* This is the launch of Add/Remove Page of Heroku App
+*/
+
+class Add_Remove_Element_Page{
+   constructor(){
+       console.log("Welcome to the Add/Remove Page of Heroku App")
+   }
+}
