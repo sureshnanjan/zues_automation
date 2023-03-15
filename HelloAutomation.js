eeb2c73ee26904d60c9519fab34baf2d36d906d9
sureshnanjan <sureshnanjan@outlook.com>
2023-03-15 10:06:10 +0530

    Adding changes with PageObject

--- HelloAutomation.js
+++ HelloAutomation.js
@@ -1,7 +1,33 @@
 /**
  * This is Doc Examples
  */
+class HomePage{
+    constructor(){
+        this.Heading = "Identifier for the Heading"
+        this.SubHeading = "Identifier for the Sub Heading on the home page"
+        this.GotoPage = (page) => {console.log(`Accessing this page ${page}`)}
+    }}
 console.log("Automating Heroku App")
+let mypage = new HomePage();
+mypage.GotoPage("Add Remove Elements")
+
+/**
+ * This method take care of naviaging and working on Add remove elements
+ * @param {string} param1 
+ * @param {number} param2 
+ * @returns {string}
+ */
+function MyMethod(param1,param2){
+
+    return 100;
+
+}
+
+
+
+
+
+
 
 /**
  * 
@@ -11,12 +37,38 @@ function openHomePage(url){
     // Perform some operations
 }
 
+// Single Line Comment
+/* 
+dslkjkfljsadkfjsadf
+fsdaklfkadslfkf;lasff
+fskadflasflasfk;lfklff
+f
+fsadff
+Multiple line comments 
+that can span many many lines
+
+sjdfksajkj
+
+*/
 /**
- * This is the type that holding the details of the Heroku App Home Page
+ * 
+ * 
  */
-class HomePage{
-    constructor(){
-        this.Heading = "Identifier for the Heading"
-        this.SubHeading = "Identifier for the Sub Heading on the home page"
-    }
+
+
+/**
+ * This is the type to holdthe details of the Heroku App Home Page
+ */
+
+/** 
+ * This is a Demo Method for JSDoc Understanding
+ * @param {string} param1 This is the name of the first Parameyer
+ * @param {number} param2 This is the number of times the value should print
+*/
+function SureshDEmoforJSDoC(param1 , param2){
+
+    return 100;
 }
+
+SureshDEmoforJSDoC();
+
