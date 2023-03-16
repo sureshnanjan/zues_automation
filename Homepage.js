c1cb8b1282386bdff2f56cbf86c4ed9c1ab72731
Poorvahingne <poorvahingne@gmail.com>
2023-03-16 23:43:13 +0530

    Added the jsdocs for the Heroku app

---
+++ Homepage.js
@@ -0,0 +1,20 @@
+let Heading = "";
+let Subheading = "";
+
+/**
+ * Access a page in the Heroku App
+ * @param {*} name - Provide the name of the Page 
+ */
+
+function AccessPages(name){
+
+}
+
+AccessPages("AddRemove")
+let page = "Add_Remove_Element_Page";
+
+function poorvaDemoForJSDoc (param1, param2) {
+    return 50;
+}
+
+poorvaDemoForJSDoc("Number", 4);
