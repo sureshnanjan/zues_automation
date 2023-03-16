9d50826e085673cdeea8db538f0cab0256b8742c
Tanmay Agrawal <tanmay.agrawal@zeuslearning.mumbai>
2023-03-17 00:25:28 +0530

    Changes in local

---
+++ HelloAutomation.js
@@ -0,0 +1,36 @@
+/**
+ * This is Doc Examples
+ */
+console.log("Automating Heroku App")
+
+/**
+ * 
+ * @param {*} url 
+ */
+function openHomePage(url){
+    // Perform some operations
+}
+
+/**
+ * This is the type that holding the details of the Heroku App Home Page
+ */
+class DemoPage{
+    constructor(){
+        this.Heading = "Identifier for the Heading"
+        this.SubHeading = "Identifier for the Sub Heading on the home page"
+    }
+}
+
+/**
+ * This is jsdoc explanaton
+ * @param {String} param1 This is parameter 1
+ * @param {*} param2 This is parameter 2
+ * @returns 
+ */
+
+
+function Tanmayjsdoc(param1, param2){
+    return 100;
+}
+
+Tanmayjsdoc("", )
---
+++ Hellojs.js
@@ -0,0 +1,19 @@
+let objective = "This is a QA Training Programme"
+typeof(objective)
+console.log(objective)
+
+/**
+ * This is a simple addition
+ * @param {number} param1 
+ * @param {number} param2 
+ * @returns{number}
+ */
+
+function MyMethod(param1,param2)
+{
+
+    
+    return 100;
+}
+
+MyMethod()
