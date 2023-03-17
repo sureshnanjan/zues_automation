504a243ffe47f0eb6da68b72f93a21728587b766
Himanshu Tawde <himanshu.tawde@zeuslearning.mumbai>
2023-03-17 08:55:06 +0530

    JSDoc Task

---
+++ HomePage.js
@@ -0,0 +1,23 @@
+let pageHeading = 'test'; //Heading of the page
+
+function PageAccess(){
+    //Access page page
+    console.log("Test 1")
+}
+
+/**
+ * Represents a book.
+ * @constructor
+ * @param {string} pageTitle - The title of the page.
+ * @param {string} addElement - The add button on the page.
+ */
+
+function addOperation (pageTitle, addElement) {
+    //Open page
+    //Find Add button
+    //Click on Add button
+    console.log("Test 2")
+}
+
+PageAccess();
+addOperation();
