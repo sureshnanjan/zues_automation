3721dc14829706d735616b610fc4573b347c06fe
Tanmay Agrawal <tanmay.agrawal@zeuslearning.mumbai>
2023-03-17 09:51:10 +0530

    New Changes again

--- HelloAutomation.js
+++ HelloAutomation.js
@@ -14,23 +14,9 @@ function openHomePage(url){
 /**
  * This is the type that holding the details of the Heroku App Home Page
  */
-class DemoPage{
+class HomePage{
     constructor(){
         this.Heading = "Identifier for the Heading"
         this.SubHeading = "Identifier for the Sub Heading on the home page"
     }
 }
-
-/**
- * This is jsdoc explanaton
- * @param {String} param1 This is parameter 1
- * @param {*} param2 This is parameter 2
- * @returns 
- */
-
-
-function Tanmayjsdoc(param1, param2){
-    return 100;
-}
-
-Tanmayjsdoc("", )
