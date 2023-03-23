222d4b643faaddff3e679305318a292a2c9d530b
Rumi Shaikh <rumi.shaikh@zeuslearning.mumbai>
2023-03-23 22:40:51 +0530

    Adding a demo function for JSDoc

--- HelloAutomation.js
+++ HelloAutomation.js
@@ -20,3 +20,14 @@ class HomePage{
         this.SubHeading = "Identifier for the Sub Heading on the home page"
     }
 }
+
+/**
+ * This is a demo for JSDoc
+ * @param {*} param1 This is the name of first parameter
+ * @param {*} param2 This is the number of times the value should print
+ */
+ function RumiDemo(param1, param2){
+    return 200;
+}
+
+RumiDemo("Parameter 1", 100)
