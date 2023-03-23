f67196447c7b375b4863c014d7edcd2fc7025b3a
jamimanasir98 <jeme333@gmail.com>
2023-03-24 04:06:25 +0530

    JSDOC for new method & Page Objects for heroku app

--- HelloAutomation.js
+++ HelloAutomation.js
@@ -2,7 +2,8 @@
  * This is Doc Examples
  */
 console.log("Automating Heroku App")
-
+let myPage = new HomePage();
+myPageGotoPage("Add Remove Elements")
 /**
  * 
  * @param {*} url 
@@ -12,11 +13,37 @@ function openHomePage(url){
 }
 
 /**
+ * This method takes care of navigating and working on Add remove elements
+ * @param {string} param1 
+ * @param {number} param2 
+ * @returns {string}
+ */
+
+function MyMethod(param1,param2){
+    return 100;
+}
+
+MyMethod()
+
+/**
+ * This is a Demo method for JSDOC understanding
+ * @param {string} string1 This is a string parameter 
+ * @param {number} integer1 This is an integer parameter
+ */
+
+function JamimaJSDOCDemo(string1,integer1){
+    return 100;
+}
+
+JamimaJSDOC();
+
+/**
  * This is the type that holding the details of the Heroku App Home Page
  */
 class HomePage{
     constructor(){
         this.Heading = "Identifier for the Heading"
         this.SubHeading = "Identifier for the Sub Heading on the home page"
+        this.GOtoPage = (page) => {console.log(`Accessing this page ${page}`)}
     }
 }
---
+++ PageObjects/HomePage.js
@@ -0,0 +1,14 @@
+let Heading = "";
+let SubHeading = "";
+/**
+ * Access a page in the Heroku App
+ * @param {string} name - Provide the name of the Page
+ */
+function AccessPages(name){
+    // Using Plawright
+    // Launch Browser 
+    // Take the name of page and navigate to it
+}
+
+AccessPages("AddRemove")
+let page = AddRemoveElements()
