1269058e945c660efe571c4a58f0a3bdb527d15f
srushtynijai <srushtypnijai@gmail.com>
2023-03-23 23:53:03 +0530

    Changes to project

---
+++ HelloAutomation.js
@@ -0,0 +1,31 @@
+/**
+ * This is Doc Examples
+ */
+console.log("Automating Heroku App")
+
+/**
+ * 
+ * @param {*} url 
+ */
+function openHomePage(url){
+    // Perform some operations
+}
+
+/**
+ * This is the type that holding the details of the Heroku App Home Page
+ */
+class HomePage{
+    constructor(){
+        this.Heading = "Identifier for the Heading"
+        this.SubHeading = "Identifier for the Sub Heading on the home page"
+    }
+}
+let Heading = "Heading";
+/**
+ * @param {string} param1  This is a string parameter
+ * @param {int} param1  This is a integer type parameter 
+ */
+function NewDemo(param1, param2){
+    return 1;
+}
+NewDemo("ABC",1)
