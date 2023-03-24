d4f1d9e0e55b666f7f72e56072ab07e8f1bd0f92
cmakwana07 <chikumak70@gmail.com>
2023-03-24 11:08:27 +0530

    Added HelloAutomation

---
+++ HelloAutomation.js
@@ -0,0 +1,59 @@
+/**
+ * This is Doc Examples
+ */
+ class HomePage{
+    constructor(){
+        this.Heading = "Identifier for the Heading"
+        this.SubHeading = "Identifier for the Sub Heading on the home page"
+        this.GotoPage = (page) => {console.log(`Accessing this page ${page}`)}
+    }}
+console.log("Automating Heroku App")
+let mypage = new HomePage();
+mypage.GotoPage("Add Remove Elements")
+
+/**
+ * This method take care of naviaging and working on Add remove elements
+ * @param {string} param1 
+ * @param {number} param2 
+ * @returns {string}
+ */
+function MyMethod(param1,param2){
+
+    return 100;
+
+}
+
+/**
+ * 
+ * @param {*} url 
+ */
+function openHomePage(url){
+    // Perform some operations
+}
+
+// Single Line Comment
+/* 
+Multiple line comments 
+that can span many many lines
+*/
+/**
+ * 
+ * 
+ */
+
+
+/**
+ * This is the type to holdthe details of the Heroku App Home Page
+ */
+
+/** 
+ * This is a Demo Method for JSDoc Understanding
+ * @param {string} param1 This is the name of the first Parameyer
+ * @param {number} param2 This is the number of times the value should print
+*/
+function SureshDEmoforJSDoC(param1 , param2){
+
+    return 100;
+}
+
+SureshDEmoforJSDoC();
