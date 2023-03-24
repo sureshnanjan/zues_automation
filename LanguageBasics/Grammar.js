00f2fd16a72c542cbc22674a0f09b964babdd77b
srushtynijai <srushtypnijai@gmail.com>
2023-03-24 21:35:19 +0530

    Javascript datatypes session

---
+++ LanguageBasics/Grammar.js
@@ -0,0 +1,25 @@
+/**
+ * projectName is the project name of the string type
+ */
+let projectName = "Petstore";
+console.log(projectName)
+
+/** 
+ * color @type {string} 
+ *length @type {number}
+ *breadth @type {number}
+ *font @type {string}
+*/
+
+const PetStoreLogo = {color:"green",length:20,breadth:10,font:"times"}
+console.log(PetStoreLogo["color"]);
+console.log(PetStoreLogo["length"]);
+console.log(PetStoreLogo["breadth"]);
+console.log(PetStoreLogo["font"]);
+
+/**
+ * myPets is an array of pet types of the string datatype
+ */
+const myPets = ['fish','dogs','cats'];
+console.log(myPets.length);
+
