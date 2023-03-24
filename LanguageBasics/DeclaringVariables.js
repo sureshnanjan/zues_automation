e12bcbcb2872cf883dbe7a1b2eed4f7c86f9ea43
Shivaji.Darade <Shivaji.Darade@zeuslearning.mumbai>
2023-03-24 11:42:17 +0530

    Day 1 Practice - DeclaringVariables

---
+++ LanguageBasics/DeclaringVariables.js
@@ -0,0 +1,44 @@
+/**
+ * Declaring a variable and initializing it.
+ */
+let x = 42
+
+console.log('The value of x is', (x))
+/**
+ * Here we are only declaring a variable.
+ */
+let y;
+
+/**
+ * If the variable is not initialized, the default assigned value in undefined.
+ */
+console.log('The value of y is', y)
+
+/**
+ * Creating a variable with data type as Constant (read only)
+ */
+const name = "Shivaji"
+/**
+ * Printing the const data type
+ */
+console.log('My name is', name)
+
+/**
+ * Creating a array of strings
+ */
+const pets = ['dogs','cats','fish','more']
+
+/**
+ * Printing the list of items in the array.
+ */
+console.log(pets)
+
+/**
+ * Printing the first element present in the array
+ */
+console.log(pets[0])
+
+/**
+ * Printing the length of the array
+ */
+console.log(pets.length);
