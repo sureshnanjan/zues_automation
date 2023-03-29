fed7bf3827aa25948fbd02efb3d959aa1455d7ac
maithilimhatre <mhatremaithili@gmail.com>
2023-03-29 10:50:36 +0530

    Added Data types file

---
+++ DataTypes.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} x is a variable with number data type
+ * @param {string} y is a constant with string data type
+ * @param {Array} noOfpets is a variable with array data type
+ * @param {object} petslogo is a variable with object data type
+ */
+
+
+let x = 25;
+const y = "DataTypes";
+var noOfpets = [23,56,78,12];
+let petslogo = {color:"green",font:"arial",length:10,width:20};
+
+console.log(petslogo);
+console.log(petslogo.font);
+console.log(`${x} and ${y}`);
+console.log(noOfpets);
+console.log(petslogo['length']);
