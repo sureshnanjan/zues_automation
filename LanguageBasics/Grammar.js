7cca8b5d6a116cea2f1d8dc5bf532bd3bf98f07d
cmakwana07 <chikumak70@gmail.com>
2023-03-30 17:18:40 +0530

    Grammar and myfirstscript files are pushed

---
+++ LanguageBasics/Grammar.js
@@ -0,0 +1,24 @@
+const Fruh = "foobar";
+let myValue = 100; 
+// Declaration
+let myname = "chirag";
+let myspecial = "test";
+// Fruh = 100;
+myname = "another name";
+// const - Language Keywod
+// Identifier
+// Literal - 100, 88.99, 100n
+// "chirag", "suresh", "palywright"
+// true, false 
+
+console.log(`The value without init ${myValue}`);
+const PetStoreLogo = {color:"green",length:20,breadth:10,font:"times roman"}
+console.log(PetStoreLogo.length);
+console.log(PetStoreLogo["color"]);
+console.log(PetStoreLogo.breadth);
+
+// PetStoreLogo
+
+const myPets = ['fish', 'dogs','cats', 'reptiles']
+
+console.log(myPets.length)
---
+++ LanguageBasics/myfirstscript.js
@@ -0,0 +1,24 @@
+let title = "Online Training Program"
+let Objective = "QA Automation training with Playwright typescript"
+let trainername = "Suresh Nanjan"
+let participant = "Chirag"
+let participant1 = "Niraj"
+let participant2 = "Pratik"
+let participant3 = "Deepika"
+let prerequisite = "Trainer"
+let prerequisite1 = "Participants"
+let prerequisite2 = "Time"
+let prerequisite3 = "Topics"
+let prerequisite4 = "Configured machines with required tools"
+let location1 = "Remote"
+let startdate = "24th feb 2023"
+let enddate = "6th Oct 2023"
+let participantscount = 45
+console.log(`The title is ${title}`)
+console.log("Trainer name is " +trainername)
+console.log(`The particpants are ${participant}, ${participant1}, ${participant2}, and ${participant3}`)
+console.log(`This Training program requires ${prerequisite}, ${prerequisite1}, ${prerequisite2}, ${prerequisite3}, and ${prerequisite4} to complete`)
+console.log(location1)
+console.log(startdate)
+console.log(enddate)
+console.log(participantscount)
