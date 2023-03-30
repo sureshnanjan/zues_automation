66d429f4f19fd5a629b98786ec5083b8774c6038
Tanmay Agrawal <tanmay.agrawal@zeuslearning.mumbai>
2023-03-31 01:39:12 +0530

    Mocha commit changes

---
+++ Language/Gram.js
@@ -0,0 +1,23 @@
+
+const Früh = "foobar";
+let myname = "tanmay";
+let myValue;
+myname = "tanmay.agrawal";
+myValue = 20000;
+console.log(`The value without init ${myValue}`);
+
+const logo = {color:"green", length:20,breath:10,font:"times"}
+console.log(logo.color);
+console.log(logo.length);
+
+const myPets = ['dogs','cats','bird'] 
+console.log(myPets.length);
+
+let speed = 200;
+const sales = "Toyota";
+const car = { myCar: "Saturn", max: speed, special: sales };
+
+
+console.log(car.myCar); // Saturn
+console.log(car.max); // 200
+console.log(car.special); // Toyota
---
+++ Language/Object.js
@@ -0,0 +1,69 @@
+var assert = require('assert');
+describe('This is for automation testing content', function () {
+  describe('Word Repeat count', function () {
+    it('This should repeat the word number of times', function () {
+      assert.equal(repeatWord('Tanmay','2').length, 13);
+    });
+  });
+
+  describe('Displays Cube', function () {
+    it('This should display a cube of any number', function () {
+      assert.equal(cube(3),27);
+      assert.equal(cube(-3),-27);
+    });
+  });
+
+  describe('Number Multiplication', function () {
+    it('This should display a multiplication of two numbers', function () {
+      assert.equal(multiply(3,5),15);
+      assert.equal(multiply(-3,-5),15);
+    });
+  });
+});
+
+
+
+
+/**
+ * This method allows me to repeat a word that many times
+ * @param {*} word Word to repeat
+ * @param {*} times No of time to repeat word
+ */
+function repeatWord(word,times){
+    let result = ''
+    for (let index = 1; index <= times; index++) {
+
+        if (index < times) {
+            result += `${word} `
+
+        } else {
+            result += `${word}`
+
+        }
+    }
+    return result;
+}
+
+
+console.log("Testing Content")
+
+/**
+ * This method allows me to give a cube of any number
+ * @param {Integer} number Number who is going to be cubed
+ * @returns Cube of the number
+ */
+const cube = function (number) {
+  return number * number * number;
+};
+
+/**
+ * This method is two multiply 2 numbers
+ * @param {Integer} a Number 1 to be multiplied
+ * @param {Integer} b Number 2 to be multiplied
+ * @returns Multiplication of both numbers
+ */
+function multiply(a, b ) {
+  return a * b;
+}
+
+console.log(multiply(5)); // 5
