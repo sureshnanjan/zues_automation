398ccc94e65a83f36d86602cceadae6624876713
Deepika Gurav <deepika.gurav@zeuslearning.mumbai>
2023-03-31 21:00:47 +0530

    29th March session - Launguage Basics and TestCase in Mocha

---
+++ LanguageBasics/Grammer.js
@@ -0,0 +1,26 @@
+const objective = "Learning language basics";
+console.log("Objective : " + objective)
+/**const - Language Keyword
+ Identifier
+ Literal - 100, 200, 100n or "Deepika", "Suresh"
+ Boolean true, false
+*/
+
+ let name= "Deepika Gurav";
+ let age= 26; 
+ console.log(`Hi I am ${name} and my age is ${age}`);
+ 
+ 
+ /**
+  * PetStoreLogo is an object  
+  */
+ const PetStoreLogo = {color: "Green", length: 20, breadth: 30, font:"Times Roman"} 
+ console.log(PetStoreLogo.color);
+ console.log(PetStoreLogo["length"]);
+ 
+ /**
+  * myPets is an Array
+  */
+ const myPets = ['fish', 'dogs', 'cats', 'reptiles'];
+ console.log(myPets.length);
+ console.log(myPets[1]);
