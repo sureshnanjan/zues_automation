c14f1d65f6585b826c1792e9888b0acc96d3c016
maithilimhatre <mhatremaithili@gmail.com>
2023-04-08 16:05:54 +0530

    Function file with mocha test cases

---
+++ Function.js
@@ -0,0 +1,82 @@
+/**
+ * This is a basic function of greeting someone you meet.
+ * There are no parameters present.
+ */
+function Greetings1() {
+  let result3 = "Hello, hope you are doing well.";
+  console.log(result3);
+  return result3;
+}   
+
+Greetings1(); //1st line
+const result1 = Greetings1(); //2nd line
+
+/**
+ * This is a function as asking someone how they are.
+ * @param {string} name This a string parameter where a name has to be passed. 
+ */
+function Greetings2(name) {
+    result2 = console.log(`Hi, how are you ${name}?`);
+}
+Greetings2("Maithili"); //3rd line
+
+
+function loop(word,times) {
+ let result2 = "";
+        for (let index = 1; index <= times; index++) {
+            if (index<times) {
+                result2 = result2 + `${word} `; 
+            } else {
+                result2 = result2 + `${word}`;
+            }
+            
+        }
+        //console.log(result2); 
+        //console.log(result2.length); 
+ return result2;
+}
+console.log(loop("bird",4) == "bird bird bird bird"); //4th and 5th line
+console.log(loop("maithili",2).length == 18); //6th, 7th and 8th line
+
+function myFunc(theArr) {
+    theArr[0] = 30;
+    return theArr;
+  }
+  
+  const arr = [45];
+  
+  console.log(arr[0]); // 45
+  myFunc(arr);
+  console.log(arr[0]); // 30
+
+var assert = require('assert');
+describe('Description of loop function', function () {
+  describe('It should display if the length added is correct or not', function () {
+    it('Passing the function and its parameter', function () {
+      assert.equal(loop("maithili",2).length, 17);
+    });
+    it('Passing the function and its parameter', function () {
+        assert.equal(loop("alpha",3).length, 15);
+      });
+    it('MyFunc inserts valu correctly', function () {
+        const result4 = myFunc([]);
+        assert.equal(result4[0], 40);
+    });
+    it('MyFunc is not empty', function () {
+        assert.equal(myFunc([]).length, 0);
+    });
+    it('Greetings1 function testing', function () {
+      assert.equal(Greetings1().length, 20);
+  });
+  });
+});
+
+/*describe('Testing the 2nd Greetings function',function() {
+    it("Let's check it", function(){
+        assert.equal(Greetings1().length, 20);
+    });
+})
+*/
+
+
+
