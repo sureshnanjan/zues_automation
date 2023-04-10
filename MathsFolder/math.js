a0c0363c2eec5986e80aabce92ce8041fab52289
niraj.gupta <niraj.gupta@zeuslearning.com>
2023-04-10 13:28:36 +0530

    maths and packages

---
+++ MathsFolder/math.js
@@ -0,0 +1,17 @@
+// Function to add variables.
+ 
+add = (x, y) => {
+    return x + y
+}
+ 
+console.log(add(4041, 90785))
+
+function multiples(a,n){
+   // let s=0
+    //for(i=1; i<=n;n++) s=a*a
+   // return s
+    let z=0, i=0
+    while(i<n) s=a*a
+    return z
+}
+console.log(multiples(54,3))
