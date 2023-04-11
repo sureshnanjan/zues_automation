d205dfbc72bf5c0f1b82d828a0c73e4faf5ec1f0
Himanshu Tawde <himanshu.tawde@zeuslearning.mumbai>
2023-04-11 22:43:45 +0530

    Error fix Cannot read properties of undefined

--- LanguageBasics/Constructor.js
+++ LanguageBasics/Constructor.js
@@ -1,6 +1,3 @@
-var assert = require('assert');
-const {it, describe} = require('mocha');
-
 /**
  * Constructor function for pets categoies
  * @param {number} id id of the category
@@ -11,7 +8,7 @@ function Category(id, name){
     this.name = name;
     this.printCategory = function(){
         console.log(`${this.name} - ${this.id}`);
-        //return `${this.name} - ${this.id}`;
+        return `${this.name} - ${this.id}`;
     }
 }
 
@@ -136,84 +133,87 @@ for (let index = 0; index < usernames.length; index++){
 /**
  * Test suite to verify contructor functions
  */
-describe('Testing constructor functions', () => {
-    describe('Testing constructor function Category', () => {
+
+var assert = require('assert');
+
+describe('Testing constructor functions', function(){
+    describe('Testing constructor function Category', function(){
         let marine = new Category(5, 'Marine');
 
-        it('Category id should be same as input', () => {
+        it('Category id should be same as input', function(){
             assert.equal(marine.id, 5);
-        });
+        })
 
-        it('Category name should be same as input', () => {
+        it('Category name should be same as input', function(){
             assert.equal(marine.name, 'Marine');
-        });
+        })
 
-        it('The printCategory function should print category details - id and name', () => {
+        it('The printCategory function should print category details - id and name', function(){
             assert.equal(marine.printCategory(), 'Marine - 5');
-        });
-    });
+        })
+    })
 
-    describe('Testing constructor function Pet', () => {
+    describe('Testing constructor function Pet', function(){
         let turtle = new Pet(1000, 'Turtle', 'turtle.png', 'Pending');
 
-        it('Pet ID should be same as input', () => {
+        it('Pet ID should be same as input', function(){
             assert.equal(turtle.id, 1000);
-        });
+        })
 
-        it('Pet name should be same as input', () => {
+        it('Pet name should be same as input', function(){
             assert.equal(turtle.name, 'Turtle');
-        });
+        })
 
-        it('Pet photoUrl should be same as input', () => {
+        it('Pet photoUrl should be same as input', function(){
             assert.equal(turtle.imageUrl, 'turtle.png');
-        });
+        })
 
-        it('Pet status should be same as input', () => {
+        it('Pet status should be same as input', function(){
             assert.equal(turtle.status, 'Pending');
-        });
+        })
 
-        it('The printPet function should print pet details - id, name, photoUrl, status', () => {
-            assert.equal(clownfish.printPet(), '1000 - PetName -  Clownfish, PhotoUrl - turtle.png, Status - Pending');
-        });
-    });
+        it('The printPet function should print pet details - id, name, photoUrl, status', function(){
+            assert.equal(turtle.printPet(), '1000 - PetName -  Turtle, PhotoUrl - turtle.png, Status - Pending');
+        })
+    })
 
-    describe('Testing User constructor function', () => {
-        let newUser = new User(201, 'testuser3', 'Firstname3', 'Lastname3', 'testuser3@test.com', 'Password123', '3333333333', 1);
+    describe('Testing User constructor function', function(){
+        let newUser = new User(2, 'testuser3', 'Firstname3', 'Lastname3', 'testuser3@test.com', 'Password123', '3333333333', 1);
 
-        it('User id should be same as provided by the user', () => {
-            assert.equal(newUser.id, 201);
-        });
+        it('User id should be same as provided by the user', function(){
+            assert.equal(newUser.id, 2);
+        })
 
-        it(`User's username should be same as provided by the user`, () => {
+        it(`User's username should be same as provided by the user`, function(){
             assert.equal(newUser.username, 'testuser3');
-        });
+        })
 
-        it('User firstName should be same as provided by the user', () => {
+        it('User firstName should be same as provided by the user', function(){
             assert.equal(newUser.firstName, 'Firstname3');
-        });
+        })
 
-        it('User lastName should be same as provided by the user', () => {
+        it('User lastName should be same as provided by the user', function(){
             assert.equal(newUser.lastName, 'Lastname3');
-        });
+        })
 
-        it('User email should be same as provided by the user', () => {
+        it('User email should be same as provided by the user', function(){
             assert.equal(newUser.email, 'testuser3@test.com');
-        });
+        })
 
-        it('User password should be same as provided by the user', () => {
+        it('User password should be same as provided by the user', function(){
             assert.equal(newUser.password, 'Password123');
-        });
+        })
 
-        it('User phone number should be same as provided by the user', () => {
+        it('User phone number should be same as provided by the user', function(){
             assert.equal(newUser.phone, '3333333333');
-        });
+        })
 
-        it('User status should be same as provided by the user', () => {
+        it('User status should be same as provided by the user', function(){
             assert.equal(newUser.userStatus, 1);
-        });
+        })
 
-        it('The printUser function should print the id, username, firstName, lastName, email, password, phone and userStatus of the user', () => {
-            assert.equal(newUser.printUser(), '201 - testuser3, Firstname3, Lastname3, testuser3@test.com, Password123, 3333333333, 1');
-        });
-    });
-});
+        it('The printUser function should print the id, username, firstName, lastName, email, password, phone and userStatus of the user', function(){
+            assert.equal(newUser.printUser(), '2 - testuser3, Firstname3, Lastname3, testuser3@test.com, Password123, 3333333333, 1');
+        })
+    })
+})
