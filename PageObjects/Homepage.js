6a7a1c77578c65084f5066ada1ea306b519ff0fd
SHUBHAM NIGAM <nigam.shubham7@gmail.com>
2023-04-14 01:10:48 +0530

    updated helloAutomation.js and added jsdoc examples

---
+++ PageObjects/Homepage.js
@@ -0,0 +1,16 @@
+let heading ="";
+let subheading ="";
+
+/**
+ * 
+ * Access a page in the Heroku app
+ * @param {string} name Provide the name of the page
+ */
+function AccessPage(name) {
+    //using playwright
+    //Launch browser
+    //Get the url and navigate
+}
+
+AccessPages("AddRemove")
+let page = AddRemoveElements();
--- helloAutomation/helloAutomation.js
+++ helloAutomation/helloAutomation.js
@@ -1,4 +1,10 @@
-let a = 10;
-console.log(a);
+console.log("Hello Automation");
 
-console.log(5+6);
+/**
+ * 
+ * @param {string} param1 capture string
+ * @param {number} param2 capture number
+ */
+function Demo(param1, param2) {
+   return 100;
+   }
