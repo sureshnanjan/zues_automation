ea0e3baea0ce8779f746dbc62c83b8f450194cbf
SHUBHAM NIGAM <nigam.shubham7@gmail.com>
2023-04-14 01:27:14 +0530

    Added LanguageBasics file in repo

---
+++ helloAutomation/LanguageBasics/Grammar.js
@@ -0,0 +1,12 @@
+//String literal - "Hello", "Automation", "Zeus" => enclosed inside ""
+//Numeric literal - 100, 200, 300
+//const - language keyword
+
+let myname = "shubham";
+
+const alpha = "header";
+//can't change the value of aplha as it is constant variabled
+alpha = 100;
+
+//changing the name
+myname = "another name";
