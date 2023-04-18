a2edec935dbed0494dfb77cc09959a512f4c9ddb
Tanmay Agrawal <tanmay.agrawal@zeuslearning.mumbai>
2023-04-18 23:27:40 +0530

    Practise Proj 2

---
+++ typescript-starter/build/index.js
@@ -0,0 +1,4 @@
+"use strict";
+console.log('Hello world!');
+console.log('Hello world! * 2');
+console.log('Hello world! * 3');
---
+++ typescript-starter/src/index.ts
@@ -0,0 +1,4 @@
+console.log('Hello world!')
+console.log('Hello world! * 2')
+console.log('Hello world! * 3')
+console.log('Hello world! * 4')
