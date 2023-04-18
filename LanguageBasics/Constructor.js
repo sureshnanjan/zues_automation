85c85a9caaa44438a3085653b2f52fb2328f4690
neha.qazi <neha.qazi@zeuslearning.com>
2023-04-19 03:00:33 +0530

    -Assignment 2 completed

--- LanguageBasics/Constructor.js
+++ LanguageBasics/Constructor.js
@@ -130,4 +130,50 @@ user.print()
 Unit test cases for the above implementation
 */
 
-
+var assert = require('assert');
+describe('Testing petstore constructor task', function() {
+    describe('Testing instances, function () {
+    it('should create a Category instance with attributes ', function() {
+      assert.equal(fish.id, 'FI-SW-01');
+      assert.equal(fish.name, 'Angelfish');
+    });
+    it('should create a pet instance with correct properties', function() {
+        assert.equal(reptile.id,'RP-SN-01');
+        assert.equal(reptile.name,'Rattlesnake');
+        assert.equal(reptile.photoUrls,'www.google.com');
+        assert.equal(reptile.status,'In store');
+      });
+    it('should create a users instance with correct properties', function() {
+          var user1 = new user(1,
+                'JaneRizzoli',
+                'Jane',
+                'Rizzoli',
+                'janeRizzoli@gmail.com',
+                'Password',
+                '123-456-7890',
+                1
+              );
+          assert.equal(user1.id,1);
+          assert.equal(user1.username,'JaneRizzoli');
+          assert.equal(user1.firstName,'Jane');
+          assert.equal(user1.lastName,"Rizzoli");
+          assert.equal(user1.email,"janeRizzoli@gmail.com");
+          assert.equal(user1.password,"Password");
+          assert.equal(user1.phone,"123-456-7890");
+          assert.equal(user1.userStatus,1);
+          });
+    it('should create a order instance with correct properties', function() {
+        var order1 = new order(1,1,1,"2023-04-03","delivered",true);
+        assert.equal(order1.id,1);
+        assert.equal(order1.petId,1);
+        assert.equal(order1.quantity,1);
+        assert.equal(order1.shipDate,"2023-04-03");
+        assert.equal(order1.status,'delivered');
+        assert.equal(order1.complete,true);
+    });
+    it('should create a tag instance with correct properties', function() {
+        //var colourful = new tag(1,"colourful");
+        assert.equal(tags[0].id,1);
+        assert.equal(tags[0].name,"small");
+    });
+  });
