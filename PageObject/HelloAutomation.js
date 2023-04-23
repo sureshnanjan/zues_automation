a60ff257836d910478ec43fcd6838ba80d1f7e9a
Bushra Shaikh <bushra.shaikh@zeuslearning.mumbai>
2023-04-24 02:40:14 +0530

    doing changes to project

--- PageObject/HelloAutomation.js
+++
@@ -1,9 +0,0 @@
-/** 
- * This is a Demo Method for JSDoc
- * @param {string} param1 Put parameter 1
- * @param {string} param2 Put parameter 2
-*/
-function BushraDemoforJSDoC(param1 , param2){
-}
-
-BushraDemoforJSDoC("","") 
