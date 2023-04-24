dfa6181545f9e75150b05d979ea2e366220cf33f
Pooja Pedamkar <pooja.pedamka@zeuslearning.mumbai>
2023-04-25 00:25:15 +0530

    Project updated

--- Assessment_3.js
+++ Assessment_3.js
@@ -1,17 +1,62 @@
  var assert = require('assert');
  describe('Testing whether all the functions are called out properly', function () {
-     describe('Testing category function is printed', function () {
-       it('Check if number of values added in category is correct', function () {
-         assert.equal((Categories.length),2);
-       });
-      it('Check if number of values added in Tags is correct', function () {
-         assert.equal((Tags.length),2);
-       });
-       it('Check if number of values added in pet is correct', function () {
-        assert.equal((Pets.length),3);
-      });
+  describe('Testing Categories function', () => {
+    let category1 = new Category( 1,"Dogs");
+    it('Category id should be same as provided by the user', () => {
+        assert.equal(category1.categid, 1);
     });
-  }); 
+    it('Category name should be same as provided by the user', () => {
+        assert.equal(category1.categname, 'Dogs');
+    });
+    it('The printCategory function should print the name and id of the category', () => {
+        assert.equal(category1.printCategory(), '1 - Dogs');
+    });
+  });
+
+  describe('Testing Tag function', () => {
+    let Tag1 = new Tag(1,"Cute");
+    it('Tag id should be same as provided by the user', () => {
+        assert.equal(Tag1.tagid, 1);
+    });
+    it('Tag name should be same as provided by the user', () => {
+        assert.equal(Tag1.tagname, 'Cute');
+    });
+    it('The printTag function should print the name and id of the tag', () => {
+        assert.equal(Tag1.printTag(), '1 - Cute');
+    });
+  });
+
+  describe('Testing Pets function', () => {
+    let pet1 = new Pet(1,1,"Dogs","BullDog","https://petstore.octoperf.com/images/dog1.gif",1,"Angry","Available");
+    it('Pet id should be same as provided by the user', () => {
+        assert.equal(pet1.petid, 1);
+    });
+    it('Category id should be same as provided by the user', () => {
+        assert.equal(pet1.categid, 1);
+    });
+    it('Category name should be same as provided by the user', () => {
+      assert.equal(pet1.categname, 'Dogs');
+    });
+    it('Pet name should be same as provided by the user', () => {
+      assert.equal(pet1.petname, 'BullDog');
+    });
+    it('Photo Url should be same as provided by the user', () => {
+      assert.equal(pet1.photoURLS, 'https://petstore.octoperf.com/images/dog1.gif');
+    });
+   it('Tag id should be same as provided by the user', () => {
+    assert.equal(pet1.tagid, 1);
+    });
+    it('Tag name should be same as provided by the user', () => {
+      assert.equal(pet1.tagname, 'Angry');
+    });
+    it('Pet Status should be same as provided by the user', () => {
+      assert.equal(pet1.petstatus, 'Available');
+    });
+    it('The printPet function should print the details of the pet', () => {
+        assert.equal(pet1.printPet(), '1 - 1 - Dogs, BullDog , photoUrl - https://petstore.octoperf.com/images/dog1.gif , 1 - Angry, status - Available');
+    });
+  });
+}); 
   
 /**
  * Display all types of pets in the store
@@ -21,21 +66,19 @@
 function Category(categid,categname){
     this.categid = categid;
     this.categname = categname;
+    this.printCategory = function () {
+      //console.log(`${this.categid} - ${this.categname}`);
+      return `${this.categid} - ${this.categname}`;
+  }
     }
 let Categories = [
         new Category( 1,"Dogs"),
         new Category(2,"Birds")
 ] 
 
-
-
-/* let dogs = new CategoryBase(1,"Dogs");
-let birds = new CategoryBase(2,"Birds");  */
 Categories.forEach(element => {
     console.log(element);
 });
-
-
 
 /**
  * Display all types of tags for individual pet in the store
@@ -46,6 +89,10 @@ Categories.forEach(element => {
 function Tag(tagid, tagname){
     this.tagid = tagid,
    this.tagname = tagname
+   this.printTag = function () {
+    //console.log(`${this.tagid} - ${this.tagname}`);
+    return `${this.tagid} - ${this.tagname}`;
+  }
 }
 
 let Tags = [
@@ -77,15 +124,19 @@ function Pet(petid ,categid, categname, petname, photoURLS, tagid, tagname, pets
     this.photoURLS = photoURLS,
     Tag.call(this,tagid, tagname),
     this.petstatus = petstatus
+    this.printPet = function () {
+      //console.log(`${this.petid} - ${this.categid} - ${this.categname}, ${this.petname}  , photoUrl - ${this.photoURLS}, ${this.tagid} - ${this.tagname},  status - ${this.status}`);
+      return `${this.petid} - ${this.categid} - ${this.categname}, ${this.petname} , photoUrl - ${this.photoURLS} , ${this.tagid} - ${this.tagname}, status - ${this.petstatus}`;
+      }
     }
+    
 let Pets = [
     new Pet(1,1,"Dog","BullDog","https://petstore.octoperf.com/images/dog1.gif",1,"Angry","Available"),
-    new Pet(2,2,"Birds","Amazon Parrot","https://petstore.octoperf.com/images/bird2.gif","1","Cute","Sold"),
+    new Pet(2,2,"Birds","Amazon Parrot","https://petstore.octoperf.com/images/bird2.gif",1,"Cute","Sold"),
     new Pet(3,2,"Birds","Finch","https://petstore.octoperf.com/images/bird1.gif", 2 ,"Angry","Available")
     ] 
 Pets.forEach(element => {
         console.log(element);
     }); 
 
-let promise = new Promise(function (resolve, reject))
 
