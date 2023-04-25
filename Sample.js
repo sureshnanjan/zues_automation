9bb42bc5434e43fc23f8f349dbcc1a1c3f57bd44
Saba_Khan <shona.khan161@gmail.com>
2023-04-25 22:57:12 +0530

    Rearrange the folder and Completed the assignment

---
+++ Sample.js
@@ -0,0 +1,3 @@
+console.log("Hello Java");
+console.log("Hello Java");
+console.log("Hello Java");
---
+++ TrainingDashboard.js
@@ -0,0 +1,11 @@
+let strName = "Saba";
+let number = 100;
+let bigNumber = 100n;
+
+//String Interpolation Operator
+console.log(`The value is ${strName} and the type is ${typeof strName}`);
+console.log(`The value is ${bigNumber} and the type is ${typeof bigNumber}`);
+console.log(`The value is ${number} and the type is ${typeof number}`);
+for (let index = 0; index < 10; index++){
+        console.log(`The operation is done ${index} times`)
+}
