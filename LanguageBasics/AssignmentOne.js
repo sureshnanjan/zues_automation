75a282d52f7c2b76aee89d8d7c361be60a08bc5d
Jessal Manidhar <jessal.manidhar@zeuslearning.mumbai>
2023-04-25 22:36:34 +0530

    Project 1

--- LanguageBasics/AssignmentOne.js
+++ LanguageBasics/AssignmentOne.js
@@ -1,6 +1,6 @@
 /**
  * Display all types of pets in the store
- * @param {} id Category Id should be a integer
+ * @param {} id Category Id should be a b
  * @param {} name Category name should be a string
  */
 
@@ -16,21 +16,20 @@ function Category(id, name){
 let fish = new Category('FI-FW-01','Koi')
 
 //Test for Category function
-/**
+
 var assert = require('assert');
     describe('Test to verify the Category function', function() {
         it('Check the values', function() {
             assert.equal(fish.print(),"ID: FI-FW-01 & Name: Koi");
     });
 });
- */
 
 /**
  * Display all types of pets in the store
- * @param {Number} id Category Id should be a Integer
- * @param {String} name Category name should be a String 
- * @param {String} photoUrls Photo URLs should be a String
- * @param {String} status Status should be a String
+ * @param {} id Category Id should be a Integer
+ * @param {} name Category name should be a String 
+ * @param {} photoUrls Photo URLs should be a String
+ * @param {} status Status should be a String
  */
 
 function Pet(id, name, photoUrls, status){
@@ -46,21 +45,20 @@ function Pet(id, name, photoUrls, status){
 
 let reptile = new Pet('RP-SN-01','Rattlesnake','www.test.com', 'Out of Stock')
 
-//Test for Pet function
-/**
+
     describe('Test to verify the Pet function', function() {
         it('Check the values', function() {
             assert.equal(reptile.print(),"ID: RP-SN-01, Name: Rattlesnake, PhotoUrls: www.test.com, Status: Out of Stock");
     });
 });
- */
 
 
 /**
  * Display all types of pets in the store
- * @param {Number} id Category Id should be a Number
- * @param {String} name Category name should be a String
+ * @param {} id Category Id should be a Number
+ * @param {} name Category name should be a String
  */
+
 function Tag(id, name){
     this.id = id;
     this.name = name;
@@ -73,23 +71,21 @@ function Tag(id, name){
 let friendly = new Tag(10,'Dog')
 
 //Test for tag function
-/**
+
     describe('Test to verify the Tag function', function() {
         it('Check the values', function() {
             assert.equal(friendly.print(),"Tag ID: 10 & Name: Dog");
     });
 });
- */
-
 
 /**
  * Display all types of pets in the store
- * @param {Number} id Category Id should be a number
- * @param {Number} petId Category name should be a number
- * @param {Number} quantity Quantity should be a number
- * @param {Number} shippedData
- * @param {String} status Status should be a string
- * @param {Boolean} complete Complete should be a boolean
+ * @param {} id Category Id should be a number
+ * @param {} petId Category name should be a number
+ * @param {} quantity Quantity should be a number
+ * @param {} shippedData
+ * @param {} status Status should be a string
+ * @param {} complete Complete should be a boolean
  * 
  */
 function Order(id, petId, quantity, shippedDate, status, complete){
@@ -108,24 +104,23 @@ function Order(id, petId, quantity, shippedDate, status, complete){
 let orderOne = new Order(20,120,1,'5/5/2023','Placed',true)
 
 //Test for order function
-/**
+
     describe('Test to verify the Order function', function() {
         it('Check the values', function() {
             assert.equal(orderOne.print(),"ID: 20, PetID: 120, Quantity: 1, Shipped Date: 5/5/2023, Status: Placed, Complete: true");
     });
 });
- */
 
 /**
  * Display all types of pets in the store
- * @param {Number} id Category Id should be a number
- * @param {String} username Username should be a string
- * @param {String} firstName First name should be a string
- * @param {String} lastName Last name should be a string
- * @param {String} email Email should be a string
- * @param {String} password Password should be a string
- * @param {Number} phone Phone should be a number
- * @param {String} userStatus User status should be a string
+ * @param {} id Category Id should be a number
+ * @param {} username Username should be a string
+ * @param {} firstName First name should be a string
+ * @param {} lastName Last name should be a string
+ * @param {} email Email should be a string
+ * @param {} password Password should be a string
+ * @param {} phone Phone should be a number
+ * @param {} userStatus User status should be a string
  * 
  */
 
@@ -147,13 +142,11 @@ function User(id, username, firstName, lastName, email, password, phone, userSta
 let userOne = new User(001,'JessalM','Jessal','Manidhar','jessal.manidhar@zeuslearning.com','password123',1234567890,'Active')
 
 //Test for user function
-/**
     describe('Test to verify the User function', function() {
         it('Check the values', function() {
             assert.equal(userOne.print(),"ID: 001, Username: JessalM, First Name: Jessal, Last Name: Manidhar, Email: jessal.manidhar@zeuslearning.com, Password: password123, Phone: 1234567890, User Status: Active");
     });
 });
- */
     
 fish.print()
 reptile.print()
