004dfd0a95110e32fa8502176430a7100da7cbb9
Saba_Khan <shona.khan161@gmail.com>
2023-04-28 22:21:55 +0530

    Practice Project 2

---
+++ TypeScript/src/index.ts
@@ -0,0 +1,9 @@
+console.log('Hello world!')
+console.log('Hello world! Again')
+console.log('Hello world! Again')
+console.log('Hello world! Again')
+console.log('Hello world!');
+
+for (let i = 0; i < 10; i++) {
+  console.log(i)
+}
