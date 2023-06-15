ed63216e2afb6888a03c64925f3e13c0b0ae7144
sureshnanjan <sureshnanjan@outlook.com>
2023-06-15 23:03:28 +0530

    Samples To Implement

---
+++ PlaywrightImplementation/BrokenImagePage.ts
@@ -0,0 +1,16 @@
+import { IBrokenImageOps } from "../Operations/IBrokenImageOps";
+import { expect, Locator, Page } from '@playwright/test';
+
+export class BrokenImagePage implements IBrokenImageOps{
+    page : Page 
+    private readonly Image1: Locator;
+    constructor(pagearg: Page){
+        this.page = pagearg;
+        this.Image1 = this.page.getByRole("img").nth(0);
+
+    }
+
+    checkImage1(){
+        expect(this.Image1).toHaveAttribute('src','path/to/file');
+    }
+}
