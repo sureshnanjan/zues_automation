4ca68fb81382379353de5318d2fc7edc1061050f
Mital <127466002+Mital-Dodiya@users.noreply.github.com>
2023-06-24 15:07:54 +0530

    Update BasicAuth.spec.ts

--- project3/BasicAuth.spec.ts
+++ project3/BasicAuth.spec.ts
@@ -2,29 +2,13 @@ import { test, expect } from '@playwright/test';
 import { BasicAuth } from '../../training/playwrightwork/BasicAuth';
 
 
-test('Testing the Username and Password field', async ({ page }) => {
+test('Testing the basic Authientication page', async ({ page }) => {
 
+    
 const Field = new BasicAuth(page);     
-await Field.goto();                     
-await Field.CorrectData();
-await Field.InCorrectData();
-await Field.NullData();               
-
+await Field.LoadCorrectData();     
+await Field.loginPage(); 
+                
 });
 
-test('Testing the cancel button', async ({ page }) => {
 
-    const Field = new BasicAuth(page);     
-    await Field.goto();                      
-    await Field.CancelButton();            
-    
-});
-
-test('Testing the content of logged in page', async ({ page }) => {
-
-    const Field = new BasicAuth(page);    
-    await Field.goto();                     
-    await Field.loginPage();              
-    
-    });
-  
