9a50afb75cce4ca35f9986b306c46a0b6cda0993
Rumi Shaikh <rumi.shaikh@zeuslearning.mumbai>
2023-06-27 19:24:57 +0530

    Implemented the interface for Dynamic Controls

---
+++ PlaywrightImplementation/DynamicControls.ts
@@ -0,0 +1,44 @@
+import { IDynamicControls } from "../Operations/IDynamicControl";
+
+class DynamicControls implements IDynamicControls{
+    verifyHeading(expectedHeading: any) {
+        throw new Error("Method not implemented.");
+    }
+    verifyDescription() {
+        throw new Error("Method not implemented.");
+    }
+    selectCheckbox() {
+        throw new Error("Method not implemented.");
+    }
+    clickRemove() {
+        throw new Error("Method not implemented.");
+    }
+    verifyConfirmationText(expectedText: any) {
+        throw new Error("Method not implemented.");
+    }
+    verifyLoadingIndicator() {
+        throw new Error("Method not implemented.");
+    }
+    verifyLoadingIndicatorText(expectedText: any) {
+        throw new Error("Method not implemented.");
+    }
+    clickAdd() {
+        throw new Error("Method not implemented.");
+    }
+    verifyTextboxEnabled() {
+        throw new Error("Method not implemented.");
+    }
+    clickEnable() {
+        throw new Error("Method not implemented.");
+    }
+    verifyTextboxDisabled() {
+        throw new Error("Method not implemented.");
+    }
+    clickDisable() {
+        throw new Error("Method not implemented.");
+    }
+    enterText(text: any) {
+        throw new Error("Method not implemented.");
+    }
+    
+}
