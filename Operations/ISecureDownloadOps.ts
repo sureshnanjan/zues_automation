6e7dd680004b8752e3b60bddb4e53a81cf56f65d
PoojaP26 <69958724+poojapedamkar@users.noreply.github.com>
2023-06-28 11:36:05 +0530

    Made changes in Secure Download Operation File

--- Operations/ISecureDownloadOps.ts
+++ Operations/ISecureDownloadOps.ts
@@ -6,16 +6,19 @@ export interface ISecureDownloadOps{
     /**
      * After login in check the heading name is correct. 
      * Check the font size of the heading
+     * @param expected - This is used to check the expected heading on the secure file download page
      */
     CheckSecureFileDownloadHeading(expected: string): Promise<void>;
       /**
      * Click on the file to download. 
      * Wait for the download process to complete and download it in the specify folder.
-     * 
+     * @param filename- This is used to specify which file should be downloaded.
+     * @param path - This is used to notify where the file should be downloaded.
      */
     ClickDownloadFile(filename: string, path: string): Promise<void> ;
       /**
      * Check the file downloded in the correct folder.
+     * @param path - This is used to check whether the file is downloaded in correct path.
      */
     CheckDownloadFile(path: string): Promise<void>;
 }
