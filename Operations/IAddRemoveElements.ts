044f127de30828d4c152651b742001ec72de1313
Shivaji.Darade <Shivaji.Darade@zeuslearning.mumbai>
2023-06-29 20:28:18 +0530

    Added Add Remove Element and updated context menu

---
+++ Operations/IAddRemoveElements.ts
@@ -0,0 +1,24 @@
+export interface IAddRemoveElements{
+    /**
+     * This function is used to check the expected heading on Add Remove elements page
+     * @param expectedHeading - used to pass the expected heading text
+     */
+    checkHeading(expectedHeading: String): void;
+
+    /**
+     * This function is used to add elements on Add Remove elements page
+     * @param element - used to pss the elements to be added
+     */
+    addElements(element: Number): void;
+
+    /**
+     * This function is used to remove elements on Add Remove elements page
+     * @param element - used to pass the elements to be removed
+     */
+    removeElements(element: Number): void;
+
+    /**
+     * This function is used to check available elements on Add Remove elements page
+     */
+    availableElements(): void;
+}
--- Operations/IContextMenu.ts
+++ Operations/IContextMenu.ts
@@ -1,12 +1,6 @@
 export interface IContextMenu{
     
         /**
-         * This method is used to navigate to the specific link on home page.
-         * @param urlToNavigate This is used to pass the name of the link to visit.
-         */
-        gotoUrl(urlToNavigate: String): void;
-
-        /**
          * This method is used to check heading on context menu page
          * @param expectedHeading - used to pass the expected heading text.
          */
@@ -14,15 +8,13 @@ export interface IContextMenu{
 
         /**
          * This method is used to check first paragraph 1 text on context menu page
-         * @param expectedParagraph1 - used to pass the expected paragraph 1 text.
          */
-        checkParagraph1(expectedParagraph1: String): void;
+        checkParagraph1(): void;
 
         /**
          * This method is used to check first paragraph 2 text on context menu page
-         * @param expectedParagraph2 - used to pass the expected paragraph 2 text.
          */
-        checkParagraph2(expectedParagraph2: String): void;
+        checkParagraph2(): void;
 
         /**
          * This method is used to check if the dashbox is present on the context menu page
