ebc544eba39f57d8fb0f6da9cf974b861a28e78e
Mital <127466002+Mital-Dodiya@users.noreply.github.com>
2023-06-29 20:53:02 +0530

    Update InterfaceForBasicAuth.ts

--- project3/InterfaceForBasicAuth.ts
+++ project3/InterfaceForBasicAuth.ts
@@ -1,8 +1,13 @@
 export interface IBasicAuthOperation{
 
-ClickBasicAuthLink(): Promise<void>
-CheckSuccessfulHeading(expected: String): Promise<void>
-CheckSuccessfulSubHeading(expected: String): Promise<void>
-CheckUnSucessfulHeading(expected: String): Promise<void>
+ClickBasicAuthLink(): Promise<void>  // Navigate to basic URL for Basic Authenication page
+
+CheckSuccessfulHeading(expected: String): Promise<void> // Once logged in, check for header "Basic Auth"
+
+CheckSuccessfulSubHeading(expected: String): Promise<void> // Once logged in, check for sub-header "Congratulations! You must have the proper credentials"
+
+CheckUnSucessfulHeading(expected: String): Promise<void> //Check heading of "Not authorized" once logged with incorrect credentials 
+
 
 }
+
