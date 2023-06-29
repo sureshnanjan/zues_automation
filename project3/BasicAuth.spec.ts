862254113a69c0b3372346f0253d5ac404ec3c1e
Mital <127466002+Mital-Dodiya@users.noreply.github.com>
2023-06-29 20:38:14 +0530

    calling functions to test basic auth page

--- project3/BasicAuth.spec.ts
+++ project3/BasicAuth.spec.ts
@@ -2,13 +2,27 @@ import { test, expect } from '@playwright/test';
 import { BasicAuth } from '../../training/playwrightwork/BasicAuth';
 
 
-test('Testing the basic Authientication page', async ({ page }) => {
+test('Testing the basic Authientication page when login pass', async ({ page }) => {
 
-    
+const string1 = 'Basic Auth';
+const string2 = 'Congratulations! You must have the proper credentials.';  
 const Field = new BasicAuth(page);     
-await Field.LoadCorrectData();     
-await Field.loginPage(); 
+await Field.ClickBasicAuthLink();
+await Field.CheckSuccessfulHeading(string1);
+await Field.CheckSuccessfulSubHeading(string2);      
+
                 
 });
+
+
+test('Testing the basic Authientication page when login fails', async ({ page }) => {
+
+    const string = 'Not authorized'
+    const Field = new BasicAuth(page);     
+    await Field.CheckUnSucessfulHeading(string);
+                      
+});
+
+
 
 
