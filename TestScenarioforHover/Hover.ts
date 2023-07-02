a3860e99cbc20f681fa7e87cb32c18ff712cd314
Mital <127466002+Mital-Dodiya@users.noreply.github.com>
2023-07-02 18:49:22 +0530

    Update Hover.ts

--- TestScenarioforHover/Hover.ts
+++ TestScenarioforHover/Hover.ts
@@ -22,26 +22,26 @@ import { Ihover } from './Ihover';
 
         //Check for header, subheader and 3 images.
 
-        await this.page.getByRole('heading', { name: 'Hovers' });
-        await this.page.getByText('Hover over the image for additional information');
-        await this.page.getByRole('img', { name: 'User Avatar' }).first();
-        await this.page.getByRole('img', { name: 'User Avatar' }).nth(1);
-        await this.page.getByRole('img', { name: 'User Avatar' }).nth(2);
+        await this.page.getByRole('heading', { name: 'Hovers' });                              //Check for heading
+        await this.page.getByText('Hover over the image for additional information');          //Check for subheading
+        await this.page.getByRole('img', { name: 'User Avatar' }).first();                     //Check for image 1   
+        await this.page.getByRole('img', { name: 'User Avatar' }).nth(1);                      //Check for image 2  
+        await this.page.getByRole('img', { name: 'User Avatar' }).nth(2);                      //Check for image 3 
 
     }
     async HoverandCheck() {
 
         //Hover over each images and check for corresponding heading and link
 
-        await this.page.getByRole('img', { name: 'User Avatar' }).first().hover();  
-        await this.page.getByRole('heading', { name: 'name: user1' });
-        await this.page.getByRole('link', { name: 'View profile' })
-        await this.page.getByRole('img', { name: 'User Avatar' }).nth(1).hover(); 
-        await this.page.getByRole('heading', { name: 'name: user2' });
-        await this.page.getByRole('link', { name: 'View profile' })
-        await this.page.getByRole('img', { name: 'User Avatar' }).nth(2).hover();
-        await this.page.getByRole('heading', { name: 'name: user3' });
-        await this.page.getByRole('link', { name: 'View profile' })
+        await this.page.getByRole('img', { name: 'User Avatar' }).first().hover();              //Hover over image 1
+        await this.page.getByRole('heading', { name: 'name: user1' });                          //Check if image have name of user1 
+        await this.page.getByRole('link', { name: 'View profile' })                             // Check for View profile link
+        await this.page.getByRole('img', { name: 'User Avatar' }).nth(1).hover();               //Hover over image 2
+        await this.page.getByRole('heading', { name: 'name: user2' });                          //Check if image have name of user2
+        await this.page.getByRole('link', { name: 'View profile' })                             // Check for View profile link
+        await this.page.getByRole('img', { name: 'User Avatar' }).nth(2).hover();               //Hover over image 3
+        await this.page.getByRole('heading', { name: 'name: user3' });                         //Check if image have name of user3
+        await this.page.getByRole('link', { name: 'View profile' })                            // Check for View profile link
 
 
     }
