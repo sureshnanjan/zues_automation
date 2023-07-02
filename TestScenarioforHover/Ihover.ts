bdaf2063eceacf8197ab66ba912270c26b888b1e
Mital <127466002+Mital-Dodiya@users.noreply.github.com>
2023-07-02 18:51:02 +0530

    Update Ihover.ts

--- TestScenarioforHover/Ihover.ts
+++ TestScenarioforHover/Ihover.ts
@@ -1,8 +1,8 @@
 export interface Ihover{
 
-    goto(): void,
-    LocateElements(): void,
-    HoverandCheck(): void 
+    goto(): void,                       //Function to go URL of Hover page
+    LocateElements(): void,            //Function to locate heading, subheading and 3 images
+    HoverandCheck(): void              //function to hover each image and find its name and profile
 
 
 }
