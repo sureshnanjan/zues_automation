982e5f5bca51425fcd29001fa7284bcc3a4fe354
Mital <127466002+Mital-Dodiya@users.noreply.github.com>
2023-07-02 18:45:05 +0530

    Update Hover.spec.ts

--- TestScenarioforHover/Hover.spec.ts
+++ TestScenarioforHover/Hover.spec.ts
@@ -4,17 +4,17 @@ import { Hovers } from '../../training/playwrightwork/Hover';
 
 test('Position of header, description and images', async ({ page }) => {
 
-const Object = new Hovers(page);     
-await Object.goto();                      
-await Object.LocateElements();             
+const Object = new Hovers(page);     //Create an instance of the page object  
+await Object.goto();                 // Call function that can go to URL of Hover page        
+await Object.LocateElements();       //Call function that can check if page contains header, subheader and 3 images 
 
 });
   
 test('Hover each image and check the data', async ({ page }) => {
   
-    const Object = new Hovers(page);     
-    await Object.goto();                      
-    await Object.HoverandCheck();  
+    const Object = new Hovers(page);   //Create an instance of the page object  
+    await Object.goto();               // Call function that can go to URL of Hover page      
+    await Object.HoverandCheck();      //Call function that can hover over each image and find its name and profile
 });
   
    
