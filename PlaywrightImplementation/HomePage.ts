dbffe13749eb0cb9e3a93fe5455e0df4c09c3bcc
Abhishek Singh <abhishek.singh@zeuslearning.mumbai>
2023-07-03 17:42:54 +0530

    Revert "Notification Messages Scenarios and test cases"
    This reverts commit e76fd7bee7cdc2f801909417a8e5f9b0e459a29f.

--- PlaywrightImplementation/HomePage.ts
+++ PlaywrightImplementation/HomePage.ts
@@ -1,15 +1,1 @@
-import { Locator, Page, expect } from "@playwright/test";
-import { IHomePage } from "../Operations/IHomePage";
-
-export class HomePage implements IHomePage{
-    readonly page: Page;
-
-    constructor(page: Page) {
-        this.page = page;
-    }
-    async visit() {
-
-        await this.page.goto("https://the-internet.herokuapp.com/");
-
-    }     
- }
+export class HomePage implements IHomePage{}
