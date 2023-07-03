0737ee38f74896a7182b500f2f7cd5081857d2e3
sannithshetty86 <sannithshetty86@gmail.com>
2023-07-03 16:53:40 +0530

    project 3 interface and scenarios

---
+++ Project3/IRedirectLink.ts
@@ -0,0 +1,8 @@
+interface IRedirectLink{
+    headertext : String;
+    contenttext : String;
+    verifyheader(); // Verify Heading of the page
+    verfiycontenttext(); // Verify Content of the page
+    click_here(); // Verify clicking on Click here link
+    verifyredirectedlink(); // Verify redirection of Click here link
+}
