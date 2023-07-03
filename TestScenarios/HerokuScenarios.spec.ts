bb50cecd7ab8a46f6f22b46207b2be5d90114505
Shivaji.Darade <Shivaji.Darade@zeuslearning.mumbai>
2023-07-03 23:00:34 +0530

    Added test scenarios

--- TestScenarios/HerokuScenarios.spec.ts
+++ TestScenarios/HerokuScenarios.spec.ts
@@ -1,14 +1,12 @@
 import { test, expect } from '@playwright/test';
 import { HomePage } from '../PlaywrightImplementation/HomePage';
-test('Heroku App Test', async ({ page }) => {
-    // Arrange
-    const hompepage = new HomePage();
-  
-    // Act
-    hompepage.visit();
-    const result = HomePage.getExampleCount();
 
-    // Assert
-    expect(result).toBeEqueal(44);
-
+  test('Verify the page Heading', async ({ page }) => {
+    const Homepage = new HomePage();
+    const Floatingmenu = new Floatingmenu();
+    //Will visit the Herokuapp homepage
+    await Homepage.visit();
+    //Will visit the Addremoveelements page
+    await floatingmenu.visit();
+    await floatingmenu.checkHeading('Floating Menu');
   });
