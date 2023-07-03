fea678cc0fc7462c67a16f6fcf5f5b525403b379
sannithshetty86 <sannithshetty86@gmail.com>
2023-07-03 20:31:15 +0530

    sortable data table cases

--- Project3/SortDataTabel.spec.ts
+++ Project3/SortDataTabel.spec.ts
@@ -1,110 +1,231 @@
 import { test, expect } from '@playwright/test';
 
-// Test case 01: 
+// Test case 01: To verify the main heading text
 
 test('Verify the heading of the page', async ({ page }) => {
     
 })
 
-// Test case 02: 
+// Test case 02: To verify the content present under main heading
 
 test('Verify the content text of the page', async ({ page }) => {
     
 })
 
-// Test case 03: 
+// Test case 03: To verify the heading of example 1
 
 test('Verify the example 1 heading of the page', async ({ page }) => {
     
 })
 
-// Test case 04: 
+// Test case 04: To verify the content present under example 1
 
 test('Verify the content text present under example 1 of the page', async ({ page }) => {
     
 })
 
-// Test case 05: 
+// Test case 05: To verify the 1st column name under example 1
 
 test('Verify the 1st column name present under example 1 table', async ({ page }) => {
     
 })
 
-// Test case 06:
+// Test case 06: To verify the 2nd column name under example 1
 
 test('Verify the 2nd column name present under example 1 table', async ({ page }) => {
     
 })
 
-// Test case 07:
+// Test case 07: To verify the 3rd column name under example 1
 
 test('Verify the 3rd column name present under example 1 table', async ({ page }) => {
     
 })
 
-// Test case 08:
+// Test case 08: To verify the 4th column name under example 1
 
 test('Verify the 4th column name present under example 1 table', async ({ page }) => {
     
 })
 
-// Test case 09:
+// Test case 09: To verify the 5th column name under example 1
 
 test('Verify the 5th column name present under example 1 table', async ({ page }) => {
     
 })
 
-// Test case 10:
+// Test case 10: To verify the 6th column name under example 1
 
 test('Verify the 6th column name present under example 1 table', async ({ page }) => {
     
 })
 
-// Test case 11: 
+// Test case 11: To verify the heading of example 2
 
 test('Verify the example 2 heading of the page', async ({ page }) => {
     
 })
 
-// Test case 12: 
+// Test case 12: To verify the content present under example 2
 
 test('Verify the content text present under example 2 of the page', async ({ page }) => {
     
 })
 
 
-// Test case 13:
+// Test case 13: To verify the 1st column name under example 2
 
 test('Verify the 1st column name present under example 2 table', async ({ page }) => {
     
 })
 
-// Test case 14:
+// Test case 14: To verify the 2nd column name under example 2
 
 test('Verify the 2nd column name present under example 2 table', async ({ page }) => {
     
 })
 
-// Test case 15:
+// Test case 15: To verify the 3rd column name under example 2
 
 test('Verify the 3rd column name present under example 3 table', async ({ page }) => {
     
 })
 
-// Test case 16:
+// Test case 16: To verify the 4th column name under example 2
 
 test('Verify the 4th column name present under example 4 table', async ({ page }) => {
     
 })
 
-// Test case 17:
+// Test case 17: To verify the 5th column name under example 2
 
 test('Verify the 5th column name present under example 5 table', async ({ page }) => {
     
 })
 
-// Test case 18:
+// Test case 18: To verify the 6th column name under example 2
 
 test('Verify the 6th column name present under example 6 table', async ({ page }) => {
+    
+})
+
+// Test case 19: To verify if data is arranged in ascending order on clicking last name column under example 1
+
+test('Verify the order of data on clicking last name column under example 1 table', async ({ page }) => {
+    
+})
+
+// Test case 20: To verify if data is arranged in descending order on clicking last name column under example 1
+
+test('Verify the order of data on clicking last name column under example 1 table', async ({ page }) => {
+    
+})
+
+// Test case 21: To verify if data is arranged in ascending order on clicking first name column under example 1
+
+test('Verify the order of data on clicking first name column under example 1 table', async ({ page }) => {
+    
+})
+
+// Test case 22: To verify if data is arranged in descending order on clicking first name column under example 1
+
+test('Verify the order of data on clicking last name column under example 1 table', async ({ page }) => {
+    
+})
+
+// Test case 23: To verify if data is arranged in ascending order on clicking Email column under example 1
+
+test('Verify the order of data on clicking email column under example 1 table', async ({ page }) => {
+    
+})
+
+// Test case 24: To verify if data is arranged in descending order on clicking Email column under example 1
+
+test('Verify the order of data on clicking email column under example 1 table', async ({ page }) => {
+    
+})
+
+// Test case 25: To verify if data is arranged in ascending order on clicking Due column under example 1
+
+test('Verify the order of data on clicking due column under example 1 table', async ({ page }) => {
+    
+})
+
+// Test case 26: To verify if data is arranged in descending order on clicking Due column under example 1
+
+test('Verify the order of data on clicking due column under example 1 table', async ({ page }) => {
+    
+})
+
+// Test case 27: To verify if data is arranged in ascending order on clicking Website column under example 1
+
+test('Verify the order of data on clicking website column under example 1 table', async ({ page }) => {
+    
+})
+
+// Test case 28: To verify if data is arranged in descending order on clicking Website column under example 1
+
+test('Verify the order of data on clicking website column under example 1 table', async ({ page }) => {
+    
+})
+
+
+// Test case 29: To verify if data is arranged in ascending order on clicking last name column under example 2
+
+test('Verify the order of data on clicking last name column under example 2 table', async ({ page }) => {
+    
+})
+
+// Test case 30: To verify if data is arranged in descending order on clicking last name column under example 2
+
+test('Verify the order of data on clicking last name column under example 2 table', async ({ page }) => {
+    
+})
+
+// Test case 31: To verify if data is arranged in ascending order on clicking first name column under example 2
+
+test('Verify the order of data on clicking first name column under example 2 table', async ({ page }) => {
+    
+})
+
+// Test case 32: To verify if data is arranged in descending order on clicking first name column under example 2
+
+test('Verify the order of data on clicking last name column under example 2 table', async ({ page }) => {
+    
+})
+
+// Test case 33: To verify if data is arranged in ascending order on clicking Email column under example 2
+
+test('Verify the order of data on clicking email column under example 2 table', async ({ page }) => {
+    
+})
+
+// Test case 34: To verify if data is arranged in descending order on clicking Email column under example 2
+
+test('Verify the order of data on clicking email column under example 2 table', async ({ page }) => {
+    
+})
+
+// Test case 35: To verify if data is arranged in ascending order on clicking Due column under example 2
+
+test('Verify the order of data on clicking due column under example 2 table', async ({ page }) => {
+    
+})
+
+// Test case 36: To verify if data is arranged in descending order on clicking Due column under example 2
+
+test('Verify the order of data on clicking due column under example 2 table', async ({ page }) => {
+    
+})
+
+// Test case 37: To verify if data is arranged in ascending order on clicking Website column under example 2
+
+test('Verify the order of data on clicking website column under example 2 table', async ({ page }) => {
+    
+})
+
+// Test case 38: To verify if data is arranged in descending order on clicking Website column under example 2
+
+test('Verify the order of data on clicking website column under example 2 table', async ({ page }) => {
     
 })
