450ecd4415ef0feb59ad3de9619a7c758602e64c
Abhishek Singh <abhishek.singh@zeuslearning.mumbai>
2023-07-03 17:42:54 +0530

    Revert "Notification Messages Scenarios and test cases"
    This reverts commit e76fd7bee7cdc2f801909417a8e5f9b0e459a29f.

--- TestScenarios/HerokuScenarios.spec.ts
+++ TestScenarios/HerokuScenarios.spec.ts
@@ -1,10 +1,14 @@
 import { test, expect } from '@playwright/test';
 import { HomePage } from '../PlaywrightImplementation/HomePage';
+test('Heroku App Test', async ({ page }) => {
+    // Arrange
+    const hompepage = new HomePage();
+  
+    // Act
+    hompepage.visit();
+    const result = HomePage.getExampleCount();
 
-test.only('Heroku App Test', async ({ page }) => {
+    // Assert
+    expect(result).toBeEqueal(44);
 
-  const Homepage = new HomePage(page);
-  //const addRemoveElement = new AddRemoveElements(page);
-  test.setTimeout(1200000);
-  await Homepage.visit('Add/Remove Element');
   });
