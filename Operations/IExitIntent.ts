75ddd2a2d3c4e9cd5dd0b850658a276c99683021
suraj.kadam <suraj.kadam@zeuslearning.com>
2023-07-14 10:04:20 +0530

    -documentation added for IEXITINTENT

--- Operations/IExitIntent.ts
+++ Operations/IExitIntent.ts
@@ -1,15 +1,44 @@
+/**
+ * Represents the Exit Intent functionality in a web page.
+ */
 export interface IExitIntent {
-    visit(): Promise<void>;
-    headertext : String;
-    contenttext : String;
-    verifyheader(); // Verify Heading of the page
-    verfiycontenttext(); // Verify Content of the page
-    checkExitIntentOverlay(): Promise<boolean>;
-    triggerExitIntent(): Promise<void>;
-  }
+  /**
+   * Simulates a visit to the page.
+   * @returns A promise that resolves when the visit is completed.
+   */
+  visit(): Promise<void>;
 
-//   In this interface, we define the following methods:
+  /**
+   * The header text displayed on the exit intent overlay.
+   */
+  headertext: string;
 
-// visit(): Promise<void>: Navigates to the page where the exit intent functionality is present.
-// checkExitIntentOverlay(): Promise<boolean>: Checks if the exit intent overlay is visible.
-// triggerExitIntent(): Promise<void>: Triggers the exit intent, simulating the user leaving the page.
+  /**
+   * The content text displayed on the exit intent overlay.
+   */
+  contenttext: string;
+
+  /**
+   * Verifies the heading of the page.
+   * @returns True if the heading matches, false otherwise.
+   */
+  verifyHeader(): boolean;
+
+  /**
+   * Verifies the content of the page.
+   * @returns True if the content matches, false otherwise.
+   */
+  verifyContentText(): boolean;
+
+  /**
+   * Checks if the exit intent overlay is displayed.
+   * @returns A promise that resolves with true if the overlay is displayed, false otherwise.
+   */
+  checkExitIntentOverlay(): Promise<boolean>;
+
+  /**
+   * Triggers the exit intent behavior on the page.
+   * @returns A promise that resolves when the exit intent is triggered.
+   */
+  triggerExitIntent(): Promise<void>;
+}
