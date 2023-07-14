ccd093f6bc38652f2918bd354e2b4d8c7f124b4b
Rahul Gupta <rahul.gupta@zeuslearning.mumbai>
2023-07-14 10:26:43 +0530

    update interfaces with jsdoc

--- Operations/IHoverPage.ts
+++ Operations/IHoverPage.ts
@@ -1,7 +1,44 @@
 export interface UploadFileOperations {
+    /**
+     * @description navigate to the upload file page
+     */
     goto(): any;
+
+    /**
+     * @description verify the File Upload heading
+     */
     verifyHeading(): any;
+
+    /**
+     * @description verify the File Upload sub-heading
+     */
     verifySubheading(): any;
+
+    /**
+     * @description hover over the image
+     * @param index index of the image
+     */
     hoverOnImage(index: number): any;
-    verifyHoverStateOfImage(): any;
+
+    /**
+     * @description verify the username, view profile button 
+     * @param username username of the hovered image
+     */
+    verifyHoverStateOfImage(username: string): any;
+    
+    /**
+     * @description hover away from the image
+     * @param index index of the image
+     */
+    hoverAwayFromImage(index: number): any;
+
+    /**
+     * @description verify the hovered elements doesn't exist
+     */
+    verifyUnHoverState(): any;
+
+    /**
+     * @description click on the view profile of the hovered image
+     */
+    viewProfile(username: string): any;
 }
