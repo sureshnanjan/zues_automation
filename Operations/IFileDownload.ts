798efbad54d690b6a64809f94aebd305f28abf07
niraj.gupta <niraj.gupta@zeuslearning.com>
2023-07-17 11:14:10 +0530

    comments for interfaces

--- Operations/IFileDownload.ts
+++ Operations/IFileDownload.ts
@@ -1,8 +1,18 @@
 export interface IFileDownload{
    
     visit(): Promise<void>;
+    
+    /* The `checkHeading` method is a function that takes in a parameter
+    `expectedHeadingFileDownloadPage` of type `string`. It is used to check if the heading on the
+    file download page matches the expected heading. */
     checkHeading(expectedHeadingFileDownloadPage: string): void;
+    
+    /*  It is used to check if a link is present on the file download page. */
     checkforLink(): void;
+
+    /* It is used to initiate the file download process. */
     dowloadFile(): void;
+
+    /*It is used to check if the file download process has completed successfully. */
     checkFileDownload(): void;
 }
--- Operations/IHorizontalSlider.ts
+++ Operations/IHorizontalSlider.ts
@@ -1,10 +1,28 @@
 export interface IHorizontalSlider{
    
     goto(): Promise<void>;
+
+    /* The `checkHeading` method is a function that takes in a parameter `expectedHeadingContextPage`
+    of type string. It is used to check if the heading on the context page matches the expected
+    heading. */
     checkHeading(expectedHeadingContextPage: string): void;
+
+    /* The `checkSliderSubHeading` method is a function that takes in a parameter `expectedParagraph`
+    of type string. It is used to check if the subheading of the slider matches the expected
+    paragraph. */
     checkSliderSubHeading(expectedParagraph: string): void;
+
+    /*  It is used to check the state of the slider. */
     checkSlider(): void;
+
+
+    /*  This method can be used to interact with the slider and trigger any associated events or actions. */
     clickSlider(): void;
+
+
+    /*  It is used to check the number associated with the slider. This
+    method can be used to verify if the number displayed on the slider is correct or if it is in the
+    expected state. */
     checkNumber(): void;
     
 }
