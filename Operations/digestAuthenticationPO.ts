780583b18e84ec549f565cca1025498eb8138586
Relvis Buthello <relvis.buthello@zeuslearning.mumbai>
2023-07-19 18:25:14 +0530

    made document changes

--- Operations/digestAuthenticationPO.ts
+++ Operations/digestAuthenticationPO.ts
@@ -1,4 +1,16 @@
 export interface digestAuthentication {
+    /*
+    Interface for login page
+    **/
     loginPage(): Promise<void>;
+
+    /*
+    Interface for alert
+    **/
     alertData(): void;
+
+    /*
+    Interface for once the user has logged in successuly
+    **/
+    successPage(): void;
 }
