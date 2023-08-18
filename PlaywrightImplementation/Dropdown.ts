a47b4fcb4f275130321d4c756f46f23ea68f6104
Rumi Shaikh <rumi.shaikh@zeuslearning.mumbai>
2023-08-18 10:02:14 +0530

    Project 3 - Implemented Dropdown

--- PlaywrightImplementation/Dropdown.ts
+++ PlaywrightImplementation/Dropdown.ts
@@ -1,17 +1,40 @@
-import { IDropdown } from "../Operations/IDropdownOps";
+import { IDropdownList } from "../Operations/IDropdownOps";
+import { Page, expect, Locator } from "@playwright/test"
 
-class Dropdown implements IDropdown{
-    verifyPlaceholderText(expectedText: string) {
-        throw new Error("Method not implemented.");
+export class Dropdown implements IDropdownList{
+    readonly page: Page;
+    readonly heading: Locator;
+    readonly dropdown: Locator;
+    readonly placeholder: Locator;
+    readonly dropdownOptions: Locator;
+    readonly selectedOption: Locator;
+
+    constructor(page: Page){
+        this.page = page
+        this.heading = page.locator('h3')
+        this.dropdown = page.locator('select')
+        this.placeholder = page.locator('//*[@id="dropdown"]/option[1]')
+        this.dropdownOptions = page.locator('option')
+        this.selectedOption = page.locator('option[selected="selected"]')
     }
-    verifyHeading(expectedHeading: string) {
-        throw new Error("Method not implemented.");
+
+    async checkHeading(headingText: string) {
+        await expect(this.heading).toHaveText(headingText)
     }
-    selectOption(option: string) {
-        throw new Error("Method not implemented.");
+    async verifyVisibilityOfDropdown() {
+        expect(await this.dropdown.isVisible()).toBeTruthy()
     }
-    verifySelectedOption() {
-        throw new Error("Method not implemented.");
+    async checkPlaceholderTextOfDropdown(placeholderText: string) {
+        await expect(this.placeholder).toHaveText(placeholderText)
+    }
+    async verifyOptionsCount(noOfOptions: number) {
+        expect(Number(await this.dropdownOptions.count())-1).toBe(noOfOptions)
+    }
+    async selectOptionFromDropdown(option: string) {
+        await this.dropdown.selectOption(option)
+    }
+    async verifySelectedOptionFromDropdown(option: string) {
+        await expect(this.selectedOption).toHaveText(option)
     }
     
 }
