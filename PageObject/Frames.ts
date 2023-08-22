b9293e92645371e98664e74db9ed24851bc5d7fd
cmakwana07 <chikumak70@gmail.com>
2023-08-22 19:38:43 +0530

    Frames implementation and test scenarios has been committed

--- PageObject/Frames.ts
+++ PageObject/Frames.ts
@@ -3,7 +3,7 @@ import { IFrames } from '../PlaywrightImplementation/IFrames';
 
 // This is my HomePage behavior
 
-export class iFrames implements iFrames{
+export class Frames implements IFrames {
     readonly page: Page;
     readonly Heading: Locator;
     readonly Link1: Locator;
@@ -11,32 +11,37 @@ export class iFrames implements iFrames{
 
     /**
     * This is the class constructor
-    * Intializing the main page 
+    * Intializing the main page
     * Intializing DialogHeading locator
     * Intializing DialogClose locator
     * Intializing ClickHere locator
     */
+    constructor(pagearg: Page) {
+        this.page = pagearg;
+        this.Heading = this.page.locator('h3', { hasText: "Frames" });
+        this.Link1 = this.page.locator('a', { hasText: "NestedFrames" });
+        this.Link2 = this.page.locator('a', { hasText: "iFrames" });
+    }
+    async visit(): Promise<void> {
+        await this.page.goto('https://the-internet.herokuapp.com/frames');
+    }
 
-    constructor(page: Page){
-        this.page = page;
-        this.Heading = page.locator('h3',{hasText:"Frames"});
-        this.Link1 = page.locator('a', {hasText:"NestedFrames"});
-        this.Link2 = page.locator('a',{hasText:"iFrames"}); 
-    }  
-async visit(){
-    await this.page.goto('https://the-internet.herokuapp.com/frames')
-}
+    async checkHeading(expectedHeadingFramesPage: string): Promise<void> {
+        await expect(this.Heading).toBeVisible();
+    }
 
-async checkHeading(){
-    await expect(this.Heading).toBeVisible();
-}
+    async checkLinkcount(nooflinks: number): Promise<void> {
+        const result = await this.page.getByRole('link');
+        await expect(result).toHaveCOunt(nooflinks);
+    }
 
-async checkLinkcount(nooflinks: number){
-    const result = await this.page.getByRole('Links')
-    await expect(result).toHaveCOunt(nooflinks);
-}
+    async gotoExample1(expectedURLnestedframesPage: string): Promise<void> {
+        await this.page.getByRole('link').click();
+        await expect(URL).toBe('https://the-internet.herokuapp.com/nested_frames');
+    }
+    async gotoExample2(expectedHeadingiFramesPage: string): Promise<void> {
+        await this.page.getByRole('link').click();
+        await expect(this.Heading).toBe("An iFrame containing the TinyMCE WYSIWYG Editor");
+    }
 
-async gotoExample(): Promise<void> {
-    await this.page.getByRole('link').click();
-}
 }
--- TestScenarios/Frames.spec.ts
+++ TestScenarios/Frames.spec.ts
@@ -1,37 +1,52 @@
 import { Page, BrowserContext, test, expect } from '@playwright/test';
-import { Frames } from '../PageObject/Frames'
+import { Frames } from "../PageObject/Frames";
 
-// Test case 01: To verify link count in the page
+//  Test case 01: To verify link count in the page
 test('Verify the no. of links available', async ({ page }) => {
-    // Step 1: Launch the link
-    // const HomePage = new iHomepage
-    // const Frame = new iFramepage
-    // Homepage.gotoexample{'Frames'}
-    // Frame.checklinkcount(2)
+    const frames = new Frames(page);
+    const expectedHeadingFramesPage="Frames";
+    await frames.visit();
+    await frames.checkHeading(expectedHeadingFramesPage);
 })
 
-//  Test case 02: To verify iFrames example is clickable
+// Test case 02: To verify link count in the page
+test('Verify the no. of links available', async ({ page }) => {
+    const frames = new Frames(page);
+    await frames.visit();
+    await frames.checkLinkcount(2);
+})
+
+//  Test case 03: To verify iFrames example is clickable
 test('Verify the iFrame example is clickable', async ({ page }) => {
-    // Step 1: Launch the link
-    // const HomePage = new iHomepage
-    // const Frame = new iFramepage
-    // Homepage.gotoexample{'Frames'}
-    // 
+    const frames = new Frames(page);
+    await frames.visit();
+    await frames.Link1.click();
 })
 
-//  Test case 03: To verify Nested Frames example is clickable
+//  Test case 04: To verify Nested Frames example is clickable
 test('Verify the Nested Frame is clickable', async ({ page }) => {
-    
+    const frames = new Frames(page);
+    await frames.visit();
+    await frames.Link2.click();
 })
 
-//  Test case 04: To verify user has navigated to the correct iFrame link
+//  Test case 05: To verify user has navigated to the correct iFrame link
 test('To check the iFrame link has correctly loaded or not', async ({ page }) => {
-    
+    const frames = new Frames(page);
+    await frames.visit();
+    await frames.Link1.click();
+    await frames.gotoExample1(expectedURLnestedframesPage);
 })
 
-//  Test case 05: To verify user has navigated to the correct Nested Frame link
+//  Test case 06: To verify user has navigated to the correct Nested Frame link
 test('To check the Nested Frame link has correctly loaded or not', async ({ page }) => {
-    
-})
+    const frames = new Frames(page);
+    await frames.visit();
+    await frames.Link1.click();
+    await frames.gotoExample2(expectedHeadingiFramesPage);
+}) 
+
+
+
 
 
