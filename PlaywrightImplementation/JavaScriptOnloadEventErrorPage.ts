02b488c4602ea9406aad2675f46acf2b0e7b0b9e
Saba_Khan <shona.khan161@gmail.com>
2023-08-30 09:55:43 +0530

    cleanup

--- PlaywrightImplementation/JavaScriptOnloadEventErrorPage.ts
+++
@@ -1,8 +0,0 @@
-import { onLoadError } from "../Operations/JavaScriptOnloadEventErrorOps";
-
-export class errorTextMessage implements onLoadError{
-    errorTextMessage: String;
-    verifyerrorText(): void {
-        throw new Error("Method not implemented.");
-    }
-}
